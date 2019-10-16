c256f4644301f95a455d9b3e301e3a9905e7fbc7
Bob Kerns <rwk@acm.org>
2019-10-16 02:16:38 -0700

    Allow instantiation to enable unit tests. Add a bit of doc.

--- src/idgen.ts
+++ src/idgen.ts
@@ -1,17 +1,28 @@
 // Copyright  by Bob Kerns. Licensed under MIT license
 
+/**
+ * Generator of ID values, either as a series of numbers, or in the form _prefix_-_number_.
+ * If a prefix is used, each prefix will count a distinct series of numbers.
+ */
 export class IdGenerator {
-    static gens = {};
-    static *idGenerator(start = 0) {
+    /**
+     * The default instance for invocation as a static singleton.
+     * */
+    private static instance = new IdGenerator();
+    private gens: {[k: string]: IterableIterator<number>} = {};
+    private *idGenerator(start: number = 0) : IterableIterator<number> {
         let id = start;
         while (true) yield id++;
     }
-    static id(prefix = "") {
-        const gen = IdGenerator.gens[prefix]|| (IdGenerator.gens[prefix] = IdGenerator.idGenerator());
+    public id(prefix: string = "", separator: string = "-") : string | number {
+        const gen = this.gens[prefix]|| (this.gens[prefix] = this.idGenerator());
         const num = gen.next().value;
         if (prefix) {
-            return `${prefix}-${num}`;
+            return `${prefix}${separator}${num}`;
         }
         return num;
+    }
+    public static id(prefix: string = "", separator: string = "-") {
+        return IdGenerator.instance.id(prefix, separator);
     }
 }
